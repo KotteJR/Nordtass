aacc56d261b6130d25aec9715d970cea71f83c00
KotteJR <aleksandar.kotevski@adamass.se>
2025-07-30 14:24:29 +0200

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Header } from "./components/Header";
+import { Header } from "./Components/Header";
 import { LanguageProvider } from "./contexts/LanguageContext";
 
 const geistSans = Geist({
